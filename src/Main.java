c9e1d8a7efac17b61aff5d3c501d1081579e613d
Aizirek <aizirek@MacBook-Pro-Aizirek.local>
2022-09-24 22:44:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] mass = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        for (int i = 0; i < mass.length; i++) {
+            if (i%2==0){
+                System.out.println(mass[i]);
+            }
+
+        }
+    }
+}
